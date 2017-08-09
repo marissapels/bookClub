3be3320eeca2aafcfea941e2da8f4e08cfc58757
Marissa Pels <marissapels@gmail.com>
2017-08-09 07:59:00 -0500

    firebase updates

---
+++ models/user.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define('User', {
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        firebase: {
+            type: DataTypes.STRING,
+            allowNull:false
+        }
+    });
+
+    User.associate = function (models) {
+        User.belongsToMany(models.Group, { through: models.UserGroup });
+    };
+
+    return User;
+};
