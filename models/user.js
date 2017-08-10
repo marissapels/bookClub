924f1f8b0a522f59aafe1c84128e0deef7ee4420
Whitney Wong <whitney.wong@utexas.edu>
2017-08-10 18:41:35 -0500

    Chat updates with fix for rendering multiple messages.

--- models/user.js
+++ models/user.js
@@ -4,10 +4,10 @@ module.exports = function (sequelize, DataTypes) {
         userName: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        firebaseId : {
+            type: DataTypes.STRING,
         }
-        // firebaseId : {
-        //     type: DataTypes.String,
-        // }
     });
 
     User.associate = function (models) {
--- public/js/discussion.js
+++ public/js/discussion.js
@@ -11,35 +11,46 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // Global variables
-var chatName, chatData, username;
+var chatName, username;
 
 /****************** Firebase Chat Functions *******************/
 // On-click event to open and post to specific discussions in Firebase
 $("#discussionList").on("click", ".disc-btn", function(){
-  // Clears previous chat messages when appending
-  $("#chat-messages").html("");
-
   // Variables to store information for Firebase
   chatName = $(this).attr("data-key");
   username = "Whitney"; //Make this dynamic for group members
-
   // Create chat directories with unique names
-  chatData = database.ref("/chat/" + chatName);
-  console.log(chatData)
+  var chatData = database.ref("/chat/" + chatName);
 
-  // Chat send button listener, grabs input and pushes to firebase.
-  $("#chat-send").click(function() {
-    if ($("#chat-input").val() !== "") {
-      var message = $("#chat-input").val();
-      chatData.push({
-        name: username,
-        message: message,
-        time: firebase.database.ServerValue.TIMESTAMP
-      });
-      $("#chat-input").val("");
-    }
+  // Clears previous chat messages when appending
+  $("#chat-messages").html("");
+  chatData.orderByChild("time").off("child_added");
+
+  // Render message to page. Update chat on screen when new message detected - ordered by 'time' value
+  chatData.orderByChild("time").on("child_added", function(snapshot) {
+    $("#chat-messages").append("<p class=chatMessages><span>"+ snapshot.val().name + "</span>: " + snapshot.val().message + "</p>");
+    // Keeps div scrolled to bottom on each update.
+    $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight); 
   });
+
+  // Unbind chat when clicked off of modal. Prevent multiple messages.
+  $("#chat-send").off('click')
+    // Chat send button listener, grabs input and pushes to firebase.
+    $("#chat-send").click(function() {
+      if ($("#chat-input").val() !== "") {
+        var message = $("#chat-input").val();
+        chatData.push({
+          name: username,
+          message: message,
+          time: firebase.database.ServerValue.TIMESTAMP
+        });
+        $("#chat-input").val("");
+      }
+    });
+    
   // Chatbox input listener
+  $("#chat-input").off('keypress')
+  // Chat send keypress listener, grabs input and pushes to firebase.
   $("#chat-input").keypress(function(e) {
     if (e.keyCode === 13 && $("#chat-input").val() !== "") {
       var message = $("#chat-input").val();
@@ -51,13 +62,8 @@ $("#discussionList").on("click", ".disc-btn", function(){
       $("#chat-input").val("");
     }
   });
-  // Render message to page. Update chat on screen when new message detected - ordered by 'time' value
-  chatData.orderByChild("time").on("child_added", function(snapshot) {
-    $("#chat-messages").append("<p class=chatMessages><span>"+ snapshot.val().name + "</span>: " + snapshot.val().message + "</p>");
-    // Keeps div scrolled to bottom on each update.
-    $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight); 
-  });
 })
+
 
 
 /****************** Display Discussions *******************/
@@ -74,18 +80,47 @@ function readDiscussions(){
       var itemBody = $("<div>");
       itemBody.addClass("collapsible-body");
 
-      var discussionButton;
+
+      var discussionButton = $("<div>");
+      discussionButton.addClass("discussionBtnArea"+data[i].id);
       data[i].Discussions.forEach(function(item) {
-        discussionButton = $("<a class='waves-effect waves-light btn view-members modal-trigger disc-btn' href='#chat' data-key=chat"+item.id+">"+item.name+"</a>");
+        discussionButton.append($("<a class='waves-effect waves-light btn modal-trigger disc-btn' href='#chat' data-key=chat"+item.id+">"+item.name+"</a>"));
         itemBody.append(discussionButton);
       });
-
+      
       discussionRow.append(itemHeader, itemBody);
       $("#discussionList").append(discussionRow);
 
       $('.collapsible').collapsible();
     }
   })
+}
+
+/****************** Create New Discussions *******************/
+// Function to create new discussions
+function newDiscussion() {
+  $("#discussionList").on("click", ".add-discussion", function(event){
+    event.preventDefault();
+    var groupId = $(this).attr("data-id");
+    var queryUrl = "/api/" + groupId + "/discussions";
+    // var discussionName = {
+    //   name: $("#newInput").val().trim()
+    // }
+    // var discussionName = "Whitney";
+    var discussionName = $("#newInput").val().trim();  //For some reason, only grabs value from Group ID = 1....need to fix
+    console.log($("#newInput").val().trim());
+    
+    var newDiscBtn;
+    $.post(queryUrl, {name: discussionName}, function (data) {
+      newDiscBtn = $("<a class='waves-effect waves-light btn modal-trigger disc-btn' href='#chat' data-key=chat"+data.id+">"+data.name+"</a>");
+      $(".discussionBtnArea"+data.id).append(newDiscBtn);
+    });
+  });
+}
+
+// Function to delete a discussion
+function deleteDiscussion() {
+
 }
 
 // Function to determine active user
@@ -106,3 +141,4 @@ $(document).ready(function(){
 
 // Call functions here
 readDiscussions();
+newDiscussion();
