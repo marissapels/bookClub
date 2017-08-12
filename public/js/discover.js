8baa508b8226a0c97780362d05c79418c58e9b10
Akia Canada <akiacanada@yahoo.com>
2017-08-11 21:54:26 -0500

    upload2

--- public/js/discover.js
+++ public/js/discover.js
@@ -1,99 +1,61 @@
 $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
-
-     // //*************Initialize Firebase******************
-	var loggedIn = false;
-
-	var config = {
-	  apiKey: "AIzaSyBUVyIW2d33WHzArLsdPx3X-X39qV-SZLY",
-	  authDomain: "bookclub-ed08b.firebaseapp.com",
-	  databaseURL: "https://bookclub-ed08b.firebaseio.com",
-	  projectId: "bookclub-ed08b",
-	  storageBucket: "bookclub-ed08b.appspot.com",
-	  messagingSenderId: "874403788158"
-	};
-	firebase.initializeApp(config);
-
-	var database = firebase.database();
-
-	firebase.auth().onAuthStateChanged(function(user) {
-	  if (user) {
-	    // User is signed in.
-	    var displayName = user.displayName;
-	    var email = user.email;
-	    var uid = user.uid;
-	    var providerData = user.providerData;
-	    loggedIn = true;
-	  } else {
-	    loggedIn=false;
-	  }
-	});
-
-	var dataMethods = {
-	  //Function that logs out a user that is logged in
-	  logOut: function() {
-	      firebase.auth().signOut().then(function() {
-	      // Sign-out successful.
-	      }).catch(function(error) {
-	      // An error happened.
-	          console.log(error);
-	      });
-	  }
-	}
-
-	//Logout
-	$("#logout").on("click", function(){
-	    dataMethods.logOut();
-	});
-
-	// ****************************************************************
+    $('.collapsible').collapsible();
 
   //when a book title is selected, it shows book info on HTML
 	$(document).on("click", "#findNewBk", function(){
 		
-		// function googleBook(bookSearch) {
+		function googleBook(bookSearch) {
 		var apiKey = "AIzaSyBUVyIW2d33WHzArLsdPx3X-X39qV-SZLY";
 		var queryURL= "https://www.googleapis.com/books/v1/volumes?q=intitle:"+ bookSearch +"&key=" + apiKey;
 		
 		$(document).on("click", "#findNewBk", function(){
-			$.ajax({
-				url: queryURL,
-				method: "GET"
-			}).done(function(response){
-				console.log(response)
-				var results = response.items[0].volumeInfo;
-				$("#chooseAuthor").html("Author: "+results.authors[0]);
-				$("#chooseRate").html("Average Rating: "+results.averageRating+"/5.0");
-				$("#chooseSummary").html("Summary: "+results.description);
-				$("#chooseImage").attr("src",results.imageLinks.smallThumbnail);
-			});
-		});
-	});
-
-
-
-	//When modal button is clicked, it clears out search contents from previous search
-	$(document).on("click", "#modalButton", function(){
-		event.preventDefault();	
-	});
-
-
-	$(document).on("click", "#findNewBk", function(){ 
-		var url = "https://api.nytimes.com/svc/books/v3/lists.json";
-		url += '?' + $.param({
-		  'api-key': "159c5dc384824c7a96c2bfda77074a6e",
-		  'list': "hardcover-fiction"
-		});
+		event.preventDefault();
 		$.ajax({
-	  		url: url,
-	  		method: 'GET',
-		}).done(function(result) {
-		  console.log(result);
-		  document.getElementById("").innerHTML=result.response.docs[?].?.? 
-		}).fail(function(err) {
-	  		throw err;
+			url: queryURL,
+			method: "GET"
+		}).done(function(response){
+			console.log(response)
+			var results = response.items[0].volumeInfo;
+			$("#ttl").html(results.title);
+			$("#").html("Author: "+results.authors[0]);
+			$("#bookOneInfo").html("Summary: "+results.description);
+			$("#bookOne").attr("src",results.imageLinks.smallThumbnail);
 		});
+		});
+		}
+
+
+	$(document).on("click", "#modalButton", function(){
+		event.preventDefault();
+		
+	});
+
+	$(document).on("click", "#findBestList", function(){ 
+	event.preventDefault();
+	
+	});
+
+	
+	$(document).on("click", "#findBestList", function(){ 
+	event.preventDefault();
+	
+	var url = "https://api.nytimes.com/svc/books/v3/lists.json";
+	url += '?' + $.param({
+  'api-key': "159c5dc384824c7a96c2bfda77074a6e",
+  'list': "hardcover-fiction"
+	});
+	$.ajax({
+  		url: url,
+  		method: 'GET',
+	}).done(function(result) {
+	  console.log(result);
+	  document.getElementById("").innerHTML=result.response.docs[?].?.? 
+	}).fail(function(err) {
+  		throw err;
+	});
+	
 	});
 
 });
